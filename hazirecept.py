f6d750f765b11b7ab2da1eecfc73da86eb305c7e
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-09-21 19:33:42 +0200

    Add files via upload

---
+++ hazirecept.py
@@ -0,0 +1,8 @@
+def mart_szendo(drink):
+    print('Elő veszek 2 szelet fehér kenyér szeletet.')
+    print('Megkenem mind a kettő felét vajjal, majd rakok mind a kettő oldalára 2-2 szelet csirke mell sonkát.')
+    print('E közben elkészítek 2 főtt tojást amit miután kész, rá rakok a kenyérre.')
+    print('Reszelek a tojás tetejére edami sajtot, majd pedig a tetejére friss saláta levelet rakok.')
+    print('Rakok rá még plusszba rá uborkát és paradicsomot is.')
+    print('Miután ezekkel megvagyok tálalom az elkészített szendvicset, mellé pedig a fogyasztó választhat tetszés szerint' + drink + ' -t.')
+mart_szendo('coca_cola')
